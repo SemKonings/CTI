e746f94561b9a29b74a4d1b7087fcd8ca15a38ab
SemKonings <154596428+SemKonings@users.noreply.github.com>
2025-04-11 13:53:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Welkom bij mijn PHP App voor CTI op Azure</h1>";
+echo "<p>Deze app draait op PHP " . phpversion() . "</p>";
+
+echo "<p>Hieronder nog meer informatie over PHP!</p>";
+phpinfo();
+?>
